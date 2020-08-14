cc74311604e5d993a66b1b3c30d9abf870123192
Narcode B <f.e.l.i.p.e@icloud.com>
2020-08-14 15:00:53 +0200

    display in elm

--- src/Main.elm
+++ src/Main.elm
@@ -1,33 +1,69 @@
-module Main exposing (..)
+port module Main exposing (..)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Html exposing (Html, text, div, h1, h2, span)
+import Html.Attributes as HA
+import Json.Decode as D
 
 
 ---- MODEL ----
 
 
 type alias Model =
-    {}
+    { display1: List (Html Msg)
+    , display2: List (Html Msg)
+    , display3: List (Html Msg)
+    , display4: List (Html Msg)
+    }
+
+type alias CK_Message =
+  { key: String
+    , display: String
+    , payload: String
+  }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { display1 = [], display2 = [], display3 = [], display4 = [] }, Cmd.none )
 
+
+-- PORTS
+port messageReceiver : (String -> msg) -> Sub msg
 
 
 ---- UPDATE ----
 
 
 type Msg
-    = NoOp
+    = Recv String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+  case msg of
+    Recv string ->
+      let
+          decoded = D.decodeString ckdecoder string
+      in
+      case decoded of
+        Ok res ->
+          let
+              wrapIt = [ span [ HA.class "codespan"] [ text res.payload ] ]
+          in
+
+          case res.display of
+            "1" -> ( {model | display1 = model.display1 ++ wrapIt }, Cmd.none )
+            "2" -> ( {model | display2 = model.display2 ++ wrapIt }, Cmd.none )
+            "3" -> ( {model | display3 = model.display3 ++ wrapIt }, Cmd.none )
+            "4" -> ( {model | display4 = model.display4 ++ wrapIt }, Cmd.none )
+            _ -> (model, Cmd.none)
+
+        Err error ->
+          Debug.log ( "Error" ++ (Debug.toString error) )
+          (model, Cmd.none)
+
+
 
 
 
@@ -36,10 +72,55 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+    div [ HA.class "container"] [
+          div [HA.class "codeheadings"] [
+            h2 [] [ text "λ functions" ]
+            , h2 [] [ text "Stack" ]
+            , h2 [] [ text "Result" ]
+            , h2 [] [ text "piano functions" ]
+          ]
+          , ( div [HA.class "codecontainer"] ( [] ++ ( makeDisplays model 4 [ span [] [] ] ) ) )
+          , div [ HA.class "imgcontainer", HA.style "background-image" "url(../images/AVeinberg.jpg)" ] []
         ]
+
+
+
+
+-- HELPER FUNCTIONS
+
+makeDisplays: Model -> Int -> List (Html Msg) -> List (Html Msg)
+makeDisplays model numOfDisplays listHtml =
+    if numOfDisplays == 0 then
+      List.reverse listHtml
+    else
+      let
+          appended = List.append listHtml [ div [ HA.id ("display" ++ String.fromInt numOfDisplays) ]
+              (getDisplayText model numOfDisplays)
+            ]
+      in
+      makeDisplays model (numOfDisplays-1) appended
+
+getDisplayText : Model -> Int -> List (Html Msg)
+getDisplayText model display =
+    case display of
+      1 -> model.display1
+      2 -> model.display2
+      3 -> model.display3
+      4 -> model.display4
+      _ -> [ span [] [] ]
+
+ckdecoder: D.Decoder CK_Message
+ckdecoder =
+    D.map3 CK_Message
+      (D.field "key" D.string)
+      (D.field "display" D.string)
+      (D.field "payload" D.string)
+
+-- SUBSCRIPTIONS
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+  messageReceiver Recv
 
 
 
@@ -52,5 +133,5 @@ main =
         { view = view
         , init = \_ -> init
         , update = update
-        , subscriptions = always Sub.none
+        , subscriptions = subscriptions
         }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
-import './main.css';
+import '../css/ckalculator.css';
 import { Elm } from './Main.elm';
 import * as serviceWorker from './serviceWorker';
 
-Elm.Main.init({
+const app = Elm.Main.init({
   node: document.getElementById('root')
 });
+
+// WebSocket
+let endpoint = '192.168.178.108' //localip address
+let ws = new WebSocket("ws://"+endpoint+":8081/ckar_consume")
+
+ws.onmessage = function(event) {
+  scrollDisplays(event.data);
+	app.ports.messageReceiver.send(event.data);
+};
+
+
+function scrollDisplays(data) {
+  let d1 = document.querySelector('#display1');
+  let d2 = document.querySelector('#display2');
+  let d3 = document.querySelector('#display3');
+  let d4 = document.querySelector('#display4');
+
+  let obj = JSON.parse(data);
+  let display = obj.display;
+
+  [d1, d2, d3, d4].forEach((d)=>{ d.scrollTop = d.scrollHeight})
+
+}
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
